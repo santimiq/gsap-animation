bdced4ecf2c2a19748a039488428e109ebabddb7
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-11 17:23:13 -0500

    Add SASS and add styles and images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import arrow from "../images/arrow.svg"
+import imgGirl from "../images/girl.webp"
+import imgBoy from "../images/boy.webp"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="hero">
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div className="hero-content-inner">
+              <h1>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Relieving the th burden</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">Of disease caused</div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">by behavior.</div>
+                </div>
+              </h1>
+              <p>Better treats serious cardiometabolic diseases to transform lives and reduce healthcare utilization
+              through the use of digital therapeutics.</p>
+              <div className="btn-row">
+                <button className="explore-button">
+                  explore
+                  <div className="arrow-icon">
+                    <img src={arrow} alt="arrow"/>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="hero-images">
+            <div className="hero-images-inner">
+              <div className="hero-image girl">
+                <img src={imgGirl} alt="girl"/>
+              </div>
+              <div className="hero-image boy">
+                <img src={imgBoy} alt="boy"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
